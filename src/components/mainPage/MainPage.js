b711b7efdeef038fc4ab7f58177ddd76a37cc26b
Dmitriy <dshishkunov.exceedteam@gmail.com>
2022-01-25 15:05:46 +0300

    add sort functional and adaptive for sort component

--- src/components/mainPage/MainPage.js
+++ src/components/mainPage/MainPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Header from "../Header"
 import Filling from "./Filling";
 import Reseptions from "./Reseptions";
+import SortReseption from './SortReseption';
 
 const MainPage = () => {
   const obj = {
@@ -13,6 +14,30 @@ const MainPage = () => {
     'Аганесов Александр Георгиевич', 
     'Белов Юрий Владимирович', 
     'Давыдов Михаил Иванович'
+  ];
+
+  const choiceSort = [
+    {
+      ru: 'Имя',
+      en: 'name'
+    },
+    {
+      ru: 'Врач',
+      en: 'doctor'
+    },
+    {
+      ru: 'Дата',
+      en: 'date'
+    },
+    {
+      ru: 'None',
+      en: 'None'
+    }
+  ];
+
+  const direction = [
+    'По возрастанию',
+    'По убыванию'
   ];
 
   const dateNow = new Date();
@@ -40,6 +65,14 @@ const MainPage = () => {
           doctors={ doctors }
           inputField={ inputField }
           setInputField={ setInputField }
+        />
+      </div>
+      <div>
+        <SortReseption 
+          choiceSort={ choiceSort }
+          direction={ direction }
+          reseptions={ reseptions }
+          setReseptions={ setReseptions }
         />
       </div>
       <div className='reseptions'>
